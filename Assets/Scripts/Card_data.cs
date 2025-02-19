2aded0b7b5702514eb9789ce170d11203ca39c6a
Thx4thefish <alsilkie5779@student.pps.net>
2025-02-19 13:23:48 -0800

    eqrt
    wert

--- Assets/Scripts/Card_data.cs
+++ Assets/Scripts/Card_data.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Card_data", menuName = "Cards/Card_data", order = 1)]
@@ -9,4 +10,5 @@ public class Card_data : ScriptableObject
     public Sprite sprite;
     public string Suit;
     public string Rank;
+    public Image spriteimage;
 }
